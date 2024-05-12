1d7bfec5ce285e5b8f8036c4a0b3a0d89ae85f60
PhantomOz <faniogor@gmail.com>
2024-05-12 18:38:43 +0100

    Create a game chess

--- src/Chess.sol
+++ src/Chess.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.20;
 
 contract Chess {
+    mapping(bytes32 => Game) private s_idToGames;
+
     enum GameType {
         Single,
         Multiplayer,
@@ -14,21 +16,56 @@ contract Chess {
         Lamante
     }
 
-    struct Game{
-        GameType mode,
-        address[2] participants,
-        BotLevel bot,
-        string turn,
-        string moves,
-        string white,
-        string black,
-        uint256 createdAt,
-        boolean gameExists
+    struct Game {
+        GameType mode;
+        address[2] participants;
+        BotLevel bot;
+        string turn;
+        string moves;
+        string white;
+        string black;
+        uint256 createdAt;
+        bool gameExists;
     }
 
-    function createGame() external {}
-    function move() external {}
-    function _isGameOver() internal {}
-    function resign() external {}
-    function getGameBoard() external {}
+    function createGame(
+        GameType _mode,
+        address _participant,
+        BotLevel _bot
+    ) external returns (bytes32 _gameId) {
+        address[2] memory _participants;
+        Game memory _game;
+        if (_mode == GameType.Single) {
+            _participants[0] = msg.sender;
+            _game.mode = _mode;
+            _game.participants = _participants;
+            _game.bot = _bot;
+            _game.turn = "w";
+            _game.moves = "";
+            _game.white = string(abi.encodePacked(msg.sender));
+            _game.black = string(abi.encodePacked(_mode));
+            _game.createdAt = block.timestamp;
+            _game.gameExists = true;
+        }
+        if (_mode == GameType.Multiplayer) {
+            _participants[0] = msg.sender;
+            _participants[1] = _participant;
+            _game.mode = _mode;
+            _game.participants = _participants;
+            _game.bot = _bot;
+            _game.turn = "w";
+            _game.moves = "";
+            _game.white = string(abi.encodePacked(msg.sender));
+            _game.black = string(abi.encodePacked(_participant));
+            _game.createdAt = block.timestamp;
+            _game.gameExists = true;
+        }
+        _gameId = keccak256(abi.encode(_game));
+        s_idToGames[_gameId] = _game;
+        return _gameId;
+    }
+    // function move() external {}
+    // function _isGameOver() internal {}
+    // function resign() external {}
+    // function getGameBoard() external {}
 }
